669ab4682b920397c0a51d3ac629f04fcf633aa7
LiyenChin <94287049+LiyenChin@users.noreply.github.com>
2022-12-19 10:41:33 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.caxdwsk0modr.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "liyen-employee"
+customregion = "us-east-1"
